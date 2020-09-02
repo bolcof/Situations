10d6167643ba0ad6d0151c91867e6ff7fd3a31b2
bolcof <sanctum.crusade.ex@gmail.com>
2020-09-02 09:47:32 +0900

    move

---
+++ Assets/Script/PlayerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float maxSpeed, speed;
+    public bool isGround = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(speed >= 0.1f)
+        {
+            speed *= 0.8f;
+        }
+        else
+        {
+            speed = 0;
+        }
+
+        if (Input.GetKey("right")) {
+            speed += 0.05f;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            speed -= 0.05f;
+        }
+
+        this.transform.position += new Vector3(speed, 0, 0);
+    }
+}
