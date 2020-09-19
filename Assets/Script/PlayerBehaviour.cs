bea0f5975e2738ce096e159bf7ff513add0f4f4e
bolcof <sanctum.crusade.ex@gmail.com>
2020-09-19 13:31:54 +0900

    base animation

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -6,6 +6,7 @@ public class PlayerBehaviour : MonoBehaviour
 {
     public float maxSpeed, speed;
     public bool isGround = false;
+    public Animator PlayerAnimator;
 
     void Start()
     {
@@ -14,24 +15,34 @@ public class PlayerBehaviour : MonoBehaviour
     
     void Update()
     {
-        if(speed >= 0.1f)
+
+        if (Input.GetKey("right")) {
+            speed += 0.006f;
+        }else if (Input.GetKey("left"))
         {
-            speed *= 0.8f;
+            speed -= 0.006f;
         }
         else
         {
+            speed *= 0.925f;
+            if (Mathf.Abs(speed) <= 0.02f)
+            {
+                speed = 0;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            PlayerAnimator.SetBool("isPraying", true);
             speed = 0;
         }
-
-        if (Input.GetKey("right")) {
-            speed += 0.05f;
-        }
-
-        if (Input.GetKey("left"))
+        else
         {
-            speed -= 0.05f;
+            PlayerAnimator.SetBool("isPraying", false);
         }
 
+
+        PlayerAnimator.SetFloat("speed", Mathf.Abs(speed));
         this.transform.position += new Vector3(speed, 0, 0);
     }
 }
