b391f7f20ddae81711c021d20aede351b2999f01
bolcof <sanctum.crusade.ex@gmail.com>
2020-09-19 23:45:16 +0900

    jump

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -7,28 +7,37 @@ public class PlayerBehaviour : MonoBehaviour
     public float maxSpeed, speed;
     public bool isGround = false;
     public Animator PlayerAnimator;
-
-    void Start()
-    {
-        
-    }
     
     void Update()
     {
 
-        if (Input.GetKey("right")) {
+        if (Input.GetKey("right") && isGround) {
             speed += 0.006f;
-        }else if (Input.GetKey("left"))
+        }else if (Input.GetKey("left") && isGround)
         {
             speed -= 0.006f;
         }
         else
         {
-            speed *= 0.925f;
+            if (!isGround)
+            {
+                speed *= 0.98f;
+            }
+            else
+            {
+                speed *= 0.925f;
+            }
+
             if (Mathf.Abs(speed) <= 0.02f)
             {
                 speed = 0;
             }
+        }
+
+        if (Input.GetKeyDown("up") && isGround)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 13.5f), ForceMode2D.Impulse);
+            isGround = false;
         }
 
         if(Input.GetKey(KeyCode.Z))
@@ -41,8 +50,25 @@ public class PlayerBehaviour : MonoBehaviour
             PlayerAnimator.SetBool("isPraying", false);
         }
 
+        this.transform.position += new Vector3(speed, 0, 0);
+        if(speed < 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(-2.25f, 2.25f, 2.25f);
+        }
+        else if(speed > 0)
+        {
+            this.gameObject.transform.localScale = new Vector3(2.25f, 2.25f, 2.25f);
+        }
 
         PlayerAnimator.SetFloat("speed", Mathf.Abs(speed));
-        this.transform.position += new Vector3(speed, 0, 0);
+        PlayerAnimator.SetBool("isGround", isGround);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGround = true;
+        }
     }
 }
